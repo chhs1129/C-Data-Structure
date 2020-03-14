7b8e65477115211dfb856d7d0753fa9acba0a909
Haosen Cheng <chhs1129@gmail.com>
2020-03-13 21:12:38 -0700

    Update myDoubleLinkedList.c

--- myDoubleLinkedList.c
+++ myDoubleLinkedList.c
@@ -18,31 +18,31 @@ int main(){
     pre = &head;
 
     for(int i = 0; i < 3; i++){
-        temp = (DDL*) malloc(sizeof(DDL));
-        temp -> value = i;
+	temp = (DDL*) malloc(sizeof(DDL));
+	temp -> value = i;
 
-        temp -> next = &head;
-        head.prev = temp;
+	temp -> next = &head;
+	head.prev = temp;
 
-        temp -> prev = pre;
-        pre -> next = temp;
+	temp -> prev = pre;
+	pre -> next = temp;
 
-        pre = temp;
+	pre = temp;
     }
     
-  // print from beginning
+    // print from beginning
     temp = head.next;								
-	while (temp != &head) {							
-		printf("forward list item: current is %p; next is %p; prev is %p; data is %d\n", temp, temp->next, temp->prev, temp->value);
-		temp = temp->next;												
-	}
+    while (temp != &head) {							
+        printf("forward list item: current is %p; next is %p; prev is %p; data is %d\n", temp, temp->next, temp->prev, temp->value);
+	temp = temp->next;												
+    }
 
-	// print from backwards
-	temp = head.prev;								
-	while (temp != &head) {							
-		printf("backward list item: current is %p; next is %p; prev is %p; data is %d\n", temp, temp->next, temp->prev, temp->value);
-		temp = temp->prev;												
-	}
+    // print from backwards
+    temp = head.prev;								
+    while (temp != &head) {							
+        printf("backward list item: current is %p; next is %p; prev is %p; data is %d\n", temp, temp->next, temp->prev, temp->value);
+	temp = temp->prev;												
+    }
 
     return 0;
 }
